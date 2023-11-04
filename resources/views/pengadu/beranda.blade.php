f0ac53485153886ff01d7033fcf7de9164a9f448
indrangrha <indran739@gmail.com>
2023-11-04 17:21:59 +0700

    mengganti format pencarian dan filter halaman beranda pada bodyrows

--- resources/views/pengadu/beranda.blade.php
+++ resources/views/pengadu/beranda.blade.php
@@ -104,7 +104,7 @@ $(document).ready(function () {
     $('#searchTerm').on('input', function () {
         var searchTerm = $(this).val();
         $.ajax({
-            url: '/search-pengadu', // Ganti dengan URL yang sesuai dengan endpoint pencarian
+            url: '/search-pengadu',
             method: 'GET',
             data: { searchTerm: searchTerm },
             success: function (data) {
@@ -122,30 +122,52 @@ $(document).ready(function () {
                             minute: 'numeric'
                         }) : '';
 
-                        var row = '<div class="col-md-10 offset-md-1"><div class="list-group"><div class="list-group-item mb-3"><div class="row"><div class="col-auto">';
-                        
-                        // Tampilkan gambar jika tersedia, jika tidak tampilkan teks "No Picture"
-                        if (result.first_image) {
-                            row += '<img class="img-fluid" src="{{ asset('storage/') }}' + result.first_image + '" alt="Photo" style="max-height: 200px;">';
-                        } else {
-                            row += '<h4>No <br>Picture</h4>';
-                        }
+                        var row = '<div class="col-md-10 offset-md-1">' +
+                            '<div class="list-group">' +
+                            '<div class="list-group-item mb-3">' +
+                            '<div class="row">' +
+                            '<div class="col-auto">';
 
-                        row += '</div><div class="col px-4">';
-                        row += '<div class="float-right">' + formattedDate + '</div>';
-                        row += '<h3>Nama Pelapor : ' + (result.anonim === 'Y' && result.id_user_fk !== '{{ auth()->user()->id }}' ? 'Anonim' : result.user.name) + '</h3>';
-                        row += '<p class="mb-2 fw-bold">Kategori : ' + result.category.name + '</p>';
-                        row += '<p class="mb-2 fw-bold">Lokasi Kejadian : ' + result.lokasi_kejadian + '</p>';
-                        row += '<p class="mb-2 fw-bold">OPD Tujuan : ' + result.opd.name + '</p>';
-                        // isi_laporan diubah menjadi string sebelum substring
-                        row += '<p class="mb-0 fw-bold">Isi Laporan : ' + String(result.isi_laporan).substring(0, 50) + '</p>';
-                        row += '<button type="button" class="btn bg-gradient-info mr-2 mt-2"><i class="fas fa-eye"></i> <a href="/detailpengaduan/' + result.id + '" style="text-decoration: none; color:white;">Detail</a></button>';
-                        row += '</div></div></div></div></div>';
+                        row += result.first_image ? '<img class="img-fluid" src="{{ asset('storage/') }}' + result.first_image + '" alt="Photo" style="max-height: 200px;">' : '<h4>No <br>Picture</h4>';
+
+                        row += '</div>' +
+                            '<div class="col px-4">' +
+                            '<div>' +
+                            '<dl class="row">' +
+                            '<dt class="col-sm-2 mb-1 pl-0">Kategori</dt>' +
+                            '<dd class="col-sm-10 mb-1"><span>:</span> ' + result.category.name + '</dd>' +
+                            '<dt class="col-sm-2 mb-1 pl-0">OPD Tujuan</dt>' +
+                            '<dd class="col-sm-10 mb-1"><span>:</span> ' + result.opd.name + '</dd>' +
+                            '<dt class="col-sm-2 pl-0">Detail Lokasi</dt>' +
+                            '<dd class="col-sm-10"><span>:</span> ' + result.lokasi_kejadian + '</dd>' +
+                            '<dt class="col-sm-2 mb-1 pl-0">Tanggal Lapor</dt>' +
+                            '<dd class="col-sm-10 mb-1"><span>:</span> ' + formattedDate + '</dd>' +
+                            '<dt class="col-sm-2 pl-0">Isi Laporan</dt>' +
+                            '<dd class="col-sm-10"><span>:</span> ' + String(result.isi_laporan).substring(0, 50) + '</dd>' +
+                            '</dl>' +
+                            '<button type="button" class="btn bg-gradient-info mr-2">' +
+                            '<i class="fas fa-eye"></i> <a href="/detailpengaduan/' + result.id + '" style="text-decoration: none; color:white;">Detail</a>' +
+                            '</button>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>';
 
                         tableBody.append(row);
+
                     });
                 } else {
-                    var noData = '<div class="col-md-10 offset-md-1"><div class="list-group-item mb-3"><div class="row"><div class="col text-center">No Data</div></div></div></div>';
+                    var noData = `
+                        <div class="col-md-10 offset-md-1">
+                            <div class="list-group-item mb-3">
+                                <div class="row">
+                                    <div class="col text-center">No Data</div>
+                                </div>
+                            </div>
+                        </div>
+                    `;
                     tableBody.append(noData);
                 }
             },
@@ -155,6 +177,7 @@ $(document).ready(function () {
         });
     });
 });
+
 </script>
 
 
@@ -181,30 +204,52 @@ $(document).ready(function () {
                             minute: 'numeric'
                         }) : '';
 
-                        var row = '<div class="col-md-10 offset-md-1"><div class="list-group"><div class="list-group-item mb-3"><div class="row"><div class="col-auto">';
-                        
-                        // Tampilkan gambar jika tersedia, jika tidak tampilkan teks "No Picture"
-                        if (result.first_image) {
-                            row += '<img class="img-fluid" src="{{ asset('storage/') }}' + result.first_image + '" alt="Photo" style="max-height: 200px;">';
-                        } else {
-                            row += '<h4>No <br>Picture</h4>';
-                        }
+                        var row = '<div class="col-md-10 offset-md-1">' +
+                            '<div class="list-group">' +
+                            '<div class="list-group-item mb-3">' +
+                            '<div class="row">' +
+                            '<div class="col-auto">';
 
-                        row += '</div><div class="col px-4">';
-                        row += '<div class="float-right">' + formattedDate + '</div>';
-                        row += '<h3>Nama Pelapor : ' + (result.anonim === 'Y' && result.id_user_fk !== '{{ auth()->user()->id }}' ? 'Anonim' : result.user.name) + '</h3>';
-                        row += '<p class="mb-2 fw-bold">Kategori : ' + result.category.name + '</p>';
-                        row += '<p class="mb-2 fw-bold">Lokasi Kejadian : ' + result.lokasi_kejadian + '</p>';
-                        row += '<p class="mb-2 fw-bold">OPD Tujuan : ' + result.opd.name + '</p>';
-                        // isi_laporan diubah menjadi string sebelum substring
-                        row += '<p class="mb-0 fw-bold">Isi Laporan : ' + String(result.isi_laporan).substring(0, 50) + '</p>';
-                        row += '<button type="button" class="btn bg-gradient-info mr-2 mt-2"><i class="fas fa-eye"></i> <a href="/detailpengaduan/' + result.id + '" style="text-decoration: none; color:white;">Detail</a></button>';
-                        row += '</div></div></div></div></div>';
+                        row += result.first_image ? '<img class="img-fluid" src="{{ asset('storage/') }}' + result.first_image + '" alt="Photo" style="max-height: 200px;">' : '<h4>No <br>Picture</h4>';
+
+                        row += '</div>' +
+                            '<div class="col px-4">' +
+                            '<div>' +
+                            '<dl class="row">' +
+                            '<dt class="col-sm-2 mb-1 pl-0">Kategori</dt>' +
+                            '<dd class="col-sm-10 mb-1"><span>:</span> ' + result.category.name + '</dd>' +
+                            '<dt class="col-sm-2 mb-1 pl-0">OPD Tujuan</dt>' +
+                            '<dd class="col-sm-10 mb-1"><span>:</span> ' + result.opd.name + '</dd>' +
+                            '<dt class="col-sm-2 pl-0">Detail Lokasi</dt>' +
+                            '<dd class="col-sm-10"><span>:</span> ' + result.lokasi_kejadian + '</dd>' +
+                            '<dt class="col-sm-2 mb-1 pl-0">Tanggal Lapor</dt>' +
+                            '<dd class="col-sm-10 mb-1"><span>:</span> ' + formattedDate + '</dd>' +
+                            '<dt class="col-sm-2 pl-0">Isi Laporan</dt>' +
+                            '<dd class="col-sm-10"><span>:</span> ' + String(result.isi_laporan).substring(0, 50) + '</dd>' +
+                            '</dl>' +
+                            '<button type="button" class="btn bg-gradient-info mr-2">' +
+                            '<i class="fas fa-eye"></i> <a href="/detailpengaduan/' + result.id + '" style="text-decoration: none; color:white;">Detail</a>' +
+                            '</button>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>';
 
                         tableBody.append(row);
+
                     });
                 } else {
-                    var noData = '<div class="col-md-10 offset-md-1"><div class="list-group-item mb-3"><div class="row"><div class="col text-center">No Data</div></div></div></div>';
+                    var noData = `
+                        <div class="col-md-10 offset-md-1">
+                            <div class="list-group-item mb-3">
+                                <div class="row">
+                                    <div class="col text-center">No Data</div>
+                                </div>
+                            </div>
+                        </div>
+                    `;
                     tableBody.append(noData);
                 }
             },
@@ -239,30 +284,52 @@ $(document).ready(function () {
                             minute: 'numeric'
                         }) : '';
 
-                        var row = '<div class="col-md-10 offset-md-1"><div class="list-group"><div class="list-group-item mb-3"><div class="row"><div class="col-auto">';
-                        
-                        // Tampilkan gambar jika tersedia, jika tidak tampilkan teks "No Picture"
-                        if (result.first_image) {
-                            row += '<img class="img-fluid" src="{{ asset('storage/') }}' + result.first_image + '" alt="Photo" style="max-height: 200px;">';
-                        } else {
-                            row += '<h4>No <br>Picture</h4>';
-                        }
+                        var row = '<div class="col-md-10 offset-md-1">' +
+                            '<div class="list-group">' +
+                            '<div class="list-group-item mb-3">' +
+                            '<div class="row">' +
+                            '<div class="col-auto">';
 
-                        row += '</div><div class="col px-4">';
-                        row += '<div class="float-right">' + formattedDate + '</div>';
-                        row += '<h3>Nama Pelapor : ' + (result.anonim === 'Y' && result.id_user_fk !== '{{ auth()->user()->id }}' ? 'Anonim' : result.user.name) + '</h3>';
-                        row += '<p class="mb-2 fw-bold">Kategori : ' + result.category.name + '</p>';
-                        row += '<p class="mb-2 fw-bold">Lokasi Kejadian : ' + result.lokasi_kejadian + '</p>';
-                        row += '<p class="mb-2 fw-bold">OPD Tujuan : ' + result.opd.name + '</p>';
-                        // isi_laporan diubah menjadi string sebelum substring
-                        row += '<p class="mb-0 fw-bold">Isi Laporan : ' + String(result.isi_laporan).substring(0, 50) + '</p>';
-                        row += '<button type="button" class="btn bg-gradient-info mr-2 mt-2"><i class="fas fa-eye"></i> <a href="/detailpengaduan/' + result.id + '" style="text-decoration: none; color:white;">Detail</a></button>';
-                        row += '</div></div></div></div></div>';
+                        row += result.first_image ? '<img class="img-fluid" src="{{ asset('storage/') }}' + result.first_image + '" alt="Photo" style="max-height: 200px;">' : '<h4>No <br>Picture</h4>';
+
+                        row += '</div>' +
+                            '<div class="col px-4">' +
+                            '<div>' +
+                            '<dl class="row">' +
+                            '<dt class="col-sm-2 mb-1 pl-0">Kategori</dt>' +
+                            '<dd class="col-sm-10 mb-1"><span>:</span> ' + result.category.name + '</dd>' +
+                            '<dt class="col-sm-2 mb-1 pl-0">OPD Tujuan</dt>' +
+                            '<dd class="col-sm-10 mb-1"><span>:</span> ' + result.opd.name + '</dd>' +
+                            '<dt class="col-sm-2 pl-0">Detail Lokasi</dt>' +
+                            '<dd class="col-sm-10"><span>:</span> ' + result.lokasi_kejadian + '</dd>' +
+                            '<dt class="col-sm-2 mb-1 pl-0">Tanggal Lapor</dt>' +
+                            '<dd class="col-sm-10 mb-1"><span>:</span> ' + formattedDate + '</dd>' +
+                            '<dt class="col-sm-2 pl-0">Isi Laporan</dt>' +
+                            '<dd class="col-sm-10"><span>:</span> ' + String(result.isi_laporan).substring(0, 50) + '</dd>' +
+                            '</dl>' +
+                            '<button type="button" class="btn bg-gradient-info mr-2">' +
+                            '<i class="fas fa-eye"></i> <a href="/detailpengaduan/' + result.id + '" style="text-decoration: none; color:white;">Detail</a>' +
+                            '</button>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>';
 
                         tableBody.append(row);
+
                     });
                 } else {
-                    var noData = '<div class="col-md-10 offset-md-1"><div class="list-group-item mb-3"><div class="row"><div class="col text-center">No Data</div></div></div></div>';
+                    var noData = `
+                        <div class="col-md-10 offset-md-1">
+                            <div class="list-group-item mb-3">
+                                <div class="row">
+                                    <div class="col text-center">No Data</div>
+                                </div>
+                            </div>
+                        </div>
+                    `;
                     tableBody.append(noData);
                 }
             },
